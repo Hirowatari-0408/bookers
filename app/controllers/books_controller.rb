c554e2b2d131a7a1713658f61a04ca0e25780ded
Hirowatari-0408 <219257853+Hirowatari-0408@users.noreply.github.com>
2025-08-02 13:53:01 +0000

    Add flash messages and error display

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,8 +18,10 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
+      flash.now[:alert] = "error"
       render :edit
     end
   end
@@ -27,8 +29,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      flash.now[:alert] = "error"
       @books = Book.all
       render :index
     end
@@ -37,6 +41,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
