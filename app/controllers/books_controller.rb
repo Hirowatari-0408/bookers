941208561a9642071ccceaa7f6008a6c2271cd26
Hirowatari-0408 <219257853+Hirowatari-0408@users.noreply.github.com>
2025-08-02 13:40:28 +0000

    Implement create action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,4 +12,20 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
