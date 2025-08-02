a94ee65d6e1edc947620ebe6f9c5900c66fa3dba
Hirowatari-0408 <219257853+Hirowatari-0408@users.noreply.github.com>
2025-08-02 13:50:51 +0000

    Implement destroy action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -34,9 +34,16 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
 end
+
